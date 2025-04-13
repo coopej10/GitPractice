2f00fc7eaecaa069a71e560071b9b6f0ed8de43a
Jessica Cooper <cooperjessica96@gmail.com>
2025-04-13 14:41:56 -0400

    Initial Commit

---
+++ Git Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git Practice
+//
+//  Created by Jessica Cooper on 4/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git Practice/Git_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_PracticeApp.swift
+//  Git Practice
+//
+//  Created by Jessica Cooper on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Git_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
